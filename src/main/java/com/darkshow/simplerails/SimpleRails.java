45c57d4b66ac75bba3939a7adf7ee528a7ec3d4d
Shovinus <baileyjoseph87@hotmail.com>
2018-11-04 10:00:57 +0000

    Initial Commit

---
+++ src/main/java/com/darkshow/simplerails/SimpleRails.java
@@ -0,0 +1,30 @@
+package com.darkshow.simplerails;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = SimpleRails.MODID, name = SimpleRails.NAME, version = SimpleRails.VERSION)
+public class SimpleRails
+{
+    public static final String MODID = "darkshow.simplerails";
+    public static final String NAME = "Simple Rails";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        
+    }
+}
